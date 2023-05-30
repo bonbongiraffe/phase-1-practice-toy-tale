f5577fd25e413709dfb78eb95767601c6c3b22dc
Sebastian Martinez <isaacsoccer235@gmail.com>
2023-05-30 01:19:50 -0500

    first one

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.getElementById('toy-collection')
+  fetch("http://localhost:3000/toys")
+    .then((r) => r.json())
+    .then((toysObj) => {
+    //  console.log(toysObj)
+      for(let i = 0; i < toysObj.length; i++) {
+        let {image, likes, name} = toysObj[i]
+       // console.log(image, likes, name)
+        let toy = document.createElement('div')
+        toy.className = 'card'
+        let toyImage = document.createElement('img')
+        toyImage.src = 'http://www.pngmart.com/files/3/Toy-Story-Woody-PNG-Photos.png'
+        
+      }
+    })
+    
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
@@ -13,3 +29,5 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+
