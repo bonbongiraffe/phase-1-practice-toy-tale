a1a34b5eaa0200f9aaccc9d6a34a2de8cefe9af3
bonbongiraffe <francescowai@yahoo.com>
2023-05-30 11:48:45 -0400

    extract add toy as function and begin add button functionality

--- src/index.js
+++ src/index.js
@@ -1,46 +1,56 @@
 let addToy = false;
+let toyLikes = [];
 
 document.addEventListener("DOMContentLoaded", () => {
   const toyCollection = document.getElementById('toy-collection')
+  function displayToy(toy){
+    let {id, image, likes, name} = toy
+    toyLikes[id] = likes
+
+    //create toy card
+    let toyCard = document.createElement('div')
+    toyCard.className = 'card'
+    
+    //toy name
+    let toyName = document.createElement('h2')
+    toyName.textContent = name
+            
+    //toy image
+    let toyImage = document.createElement('img')
+    toyImage.className = 'toy-avatar'
+    toyImage.src = image
+
+    //like counter
+    let likeCounter = document.createElement('p')
+    likeCounter.textContent = `${toyLikes[id]} likes`
+
+    //like button
+    let likeBtn = document.createElement('button')
+    likeBtn.className = 'like-btn'
+    likeBtn.textContent = 'like'
+    likeBtn.id = id
+        //likeBtn.type = 'button'
+        likeBtn.addEventListener('click', (e) => {
+          toyLikes[id]++
+          likeCounter.textContent = `${toyLikes[id]} likes`
+        })
+    
+    //append elements
+    toyCard.appendChild(toyName)
+    toyCard.appendChild(toyImage)
+    toyCard.appendChild(likeCounter)
+    toyCard.appendChild(likeBtn)
+    toyCollection.appendChild(toyCard)
+
+    return;
+  }
+
   fetch("http://localhost:3000/toys")
     .then((r) => r.json())
     .then((toysObj) => {
       //console.log(toysObj)
       for(let i = 0; i < toysObj.length; i++) {
-        //extract toy data from api
-        let {id, image, likes, name} = toysObj[i]
-        toyLikes[id] = likes
-
-        //create toy card
-        let toy = document.createElement('div')
-        toy.textContent = name
-        toy.className = 'card'
-
-        //add toy image
-        let toyImage = document.createElement('img')
-        toyImage.className = 'toy-avatar'
-        toyImage.src = image
-
-        //create like button
-        let likeBtn = document.createElement('button')
-        let likeCounter = document.createElement('div')
-        
-
-        likeCounter.textContent = `${toyLikes[id]} likes`
-        likeBtn.textContent = 'like'
-        likeBtn.id = id
-        //likeBtn.type = 'button'
-        likeBtn.addEventListener('click', (e) => {
-          toyLikes[id]++
-          likeCounter.textContent = `${toyLikes[id]} likes`
-        
-        })
-        
-        //append elements
-        toy.appendChild(toyImage)
-        toy.appendChild(likeCounter)
-        toy.appendChild(likeBtn)
-        toyCollection.appendChild(toy)
+        displayToy(toysObj[i])
       }
     })
     
@@ -55,6 +65,14 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+
+  //add button functionality
+  const toyForm = document.getElementsByClassName('add-toy-form')
+  console.log(toyForm)
+  toyForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    console.log(e)
+  })
 });
 
 
